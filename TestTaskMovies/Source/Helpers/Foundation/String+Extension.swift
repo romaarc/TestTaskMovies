47f021d7cee4a45953b8247227648b5851eb5fd0
Roman <romangorshkovreme@gmail.com>
2021-11-19 06:00:21 +0300

    fix space

--- TestTaskMovies/Source/Helpers/Foundation/String+Extension.swift
+++ TestTaskMovies/Source/Helpers/Foundation/String+Extension.swift
@@ -17,7 +17,6 @@ extension String {
         formatDate.locale = .init(identifier: "ru_RU")
         formatDate.dateFormat = "yyyy"
         let date = formatDate.string(from: backendDate)
-        
         return date
     }    
 }
